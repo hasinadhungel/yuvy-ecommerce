0e76956ee55725350ef5f49883c419b17bfc27d0
hasina1998-svg <hasinadhungel1998@gmail.com>
2022-11-07 21:27:20 +0545

    done stylling of product and category page

--- src/routing/Routers.js
+++ src/routing/Routers.js
@@ -34,6 +34,7 @@ const Routers = () => {
       <Subheader />
       <Breadcrumb />
       <div className="router-container">
+        {/* stickynavbar routing */}
         <div className="router-sidebar">
           <StickyNavbar />
         </div>
